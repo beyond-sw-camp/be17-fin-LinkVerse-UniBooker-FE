0b016d731b1d5967ed400642d9aac9a9850ba684
thay123028 <thay123028@naver.com>
2025-10-10 22:11:27 +0900

    [Fix] 중복 코드 제거

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-// 전역 스타일
-import './app.css'
-// 버튼 컴포넌트 전역 등록
-import Button from './components/Button.vue'
 
 // 전역 스타일
 import './app.css'
