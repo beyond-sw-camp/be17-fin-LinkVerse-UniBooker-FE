554cc7705ab21603656c84bcb4f0102767a6b1e1
thay123028 <thay123028@naver.com>
2025-10-10 23:50:56 +0900

    [Feat] 자주 사용하는 컬러 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx,css}"
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,css}'],
   theme: {
     extend: {
+      colors: {
+        // 블루 계열
+        primary: '#00008C',
+        'primary-back': '#1E3A8A',
+        'primary-bright': '#2563EB',
+        'primary-bbright': '#60A5FA',
+        'primary-hover': '#000070',
+
+        // 폰트 컬러
+        text: '#222222',
+
+        // 그레이 계열
+        'gray-line': '#EEEEEE',
+        'gray-deep': '#DDDDDD',
+        'gray-dark': '#555555',
+      },
       fontFamily: {
-        // 한글용
-        noto: ['Noto Sans KR'],
-        // 영문용
-        mont: ['Montserrat'],
+        'mont-noto': ['Montserrat', 'Noto Sans KR', 'sans-serif'],
       },
     },
   },
