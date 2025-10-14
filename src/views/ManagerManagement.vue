5e639a68457708af3d62a732daf2a849a81261b9
thay123028 <thay123028@naver.com>
2025-10-14 16:11:26 +0900

    [Feat] 관리자 추가 모달 구현

--- src/views/ManagerManagement.vue
+++ src/views/ManagerManagement.vue
@@ -3,9 +3,18 @@ import { ref } from 'vue'
 import AdminLayout from '@/components/AdminLayout.vue'
 
 const isEditModalOpen = ref(false)
+const isAddModalOpen = ref(false)
 
 const openEditModal = () => {
   isEditModalOpen.value = true
+}
+
+const openAddModal = () => {
+  isAddModalOpen.value = true
+}
+
+const closeAddModal = () => {
+  isAddModalOpen.value = false
 }
 </script>
 
@@ -19,7 +28,7 @@ const openEditModal = () => {
     <!-- 매니저 목록 컴포넌트 -->
     <div class="manage-list-container">
       <!-- 매니저 추가 버튼 -->
-      <div class="manage-add-button-container card">
+      <div class="manage-add-button-container card" @click="openAddModal">
         <img src="/public/assets/icons/ic-plus-circle.png" alt="매니저 추가" />
       </div>
 
@@ -59,9 +68,40 @@ const openEditModal = () => {
           <span>연락처</span>
           <input type="text" />
         </div>
-        <div class="modal-button-container">
-          <button id="delete-button">계정삭제</button>
+        <div class="edit-modal-button-container">
+          <button class="delete-button">계정삭제</button>
           <Button id="edit-button">수정</Button>
+        </div>
+      </div>
+    </Modal>
+
+    <!-- 관리자 추가 버튼 클릭 시 뜨는 모달 -->
+    <Modal :open="isAddModalOpen" @close="isAddModalOpen = false">
+      <div class="add-modal-container">
+        <h3>관리자 추가</h3>
+        <p>
+          기업의 관리자를 추가할 수 있습니다. 입력하신 이메일로 가입에 필요한 계정 정보(이메일 및
+          비밀번호)가 발송됩니다. 정확한 안내를 위해 이메일 주소를 신중하게 입력해주세요.
+        </p>
+
+        <div class="input-field-container">
+          <div class="input-field-item">
+            <label>이름 <span>*</span></label>
+            <input type="text" placeholder="관리자의 이름을 입력해주세요." />
+          </div>
+          <div class="input-field-item">
+            <label>이메일 <span>*</span></label>
+            <input type="email" placeholder="관리자의 이메일을 입력해주세요." />
+          </div>
+          <div class="input-field-item">
+            <label>연락처 <span>*</span></label>
+            <input type="phone" placeholder="관리자의 연락처를 입력해주세요." />
+          </div>
+        </div>
+
+        <div class="add-modal-button-container">
+          <button class="delete-button">취소</button>
+          <Button id="add-button">추가하기</Button>
         </div>
       </div>
     </Modal>
@@ -131,18 +171,19 @@ const openEditModal = () => {
 }
 
 .manager-info-modal P {
-    @apply text-gray-dark;
+  @apply text-gray-dark;
 }
 
-.modal-button-container {
+.edit-modal-button-container {
   @apply mt-[40px] flex gap-3;
 }
 
-.modal-button-container button {
-  @apply rounded-[5px] text-center py-[11px] flex-1 text-[14px] font-medium;
+.edit-modal-button-container button,
+.add-modal-button-container button {
+  @apply rounded-[5px] text-center py-[8px] flex-1 text-[14px] font-medium;
 }
 
-#delete-button {
+.delete-button {
   @apply bg-gray-deep text-text hover:bg-[#c8c8c8];
 }
 
@@ -150,7 +191,52 @@ const openEditModal = () => {
   @apply text-white px-0;
 }
 
-input {
-    @apply border-b border-gray-deep
+.edit-modal-button-container input {
+  @apply border-b border-gray-deep;
+}
+
+/* 관리자 추가 모달 스타일 */
+.add-modal-container {
+  @apply px-[30px] py-[20px] flex flex-col;
+}
+
+h3 {
+    @apply text-[18px] font-medium mb-[3px]
+}
+
+.add-modal-container p {
+    @apply text-[12px] text-gray-dark;
+}
+
+.input-field-container {
+    @apply mt-[35px] flex flex-col gap-6;
+}
+
+.input-field-item {
+    @apply flex flex-col gap-1;
+}
+
+label {
+    @apply text-[14px];
+}
+
+.input-field-item span {
+    @apply text-[#FF2222];
+}
+
+.add-modal-container input {
+    @apply bg-gray-line px-[14px] py-[10px] text-[14px] rounded-[3px]
+}
+
+.add-modal-button-container {
+    @apply flex gap-3 mt-[70px];
+}
+
+.add-modal-button-container button {
+    @apply py-[10px];
+}
+
+#add-button {
+    @apply px-0;
 }
 </style>
