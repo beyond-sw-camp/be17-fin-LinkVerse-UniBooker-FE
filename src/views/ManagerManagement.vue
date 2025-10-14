e5201732a6857e3f49044d7d4d3e97d44481af7e
thay123028 <thay123028@naver.com>
2025-10-14 15:31:52 +0900

    [Feat] 관리자 카드 클릭 시 수정 모달 띄우기

--- src/views/ManagerManagement.vue
+++ src/views/ManagerManagement.vue
@@ -1,5 +1,12 @@
 <script setup>
+import { ref } from 'vue'
 import AdminLayout from '@/components/AdminLayout.vue'
+
+const isEditModalOpen = ref(false)
+
+const openEditModal = () => {
+  isEditModalOpen.value = true
+}
 </script>
 
 <template>
@@ -17,63 +24,133 @@ import AdminLayout from '@/components/AdminLayout.vue'
       </div>
 
       <!-- 매니저 정보 카드 -->
-      <div class="manager-info-card card">
+      <div class="manager-info-card card" @click="openEditModal">
         <div class="card-top">
           <span>김아영</span>
           <img src="/public/assets/icons/ic-delete-trash.png" alt="매니저 삭제" />
         </div>
 
         <div class="manager-info-row">
-            <img src="/public/assets/icons/ic-email.png" alt="이메일" />
-            <span>DK8IJE32@gmail.com</span>
+          <img src="/public/assets/icons/ic-email.png" alt="이메일" />
+          <span>DK8IJE32@gmail.com</span>
         </div>
         <div class="manager-info-row">
-            <img src="/public/assets/icons/ic-phone.png" alt="연락처" />
-            <span>010-1234-5678</span>
+          <img src="/public/assets/icons/ic-phone.png" alt="연락처" />
+          <span>010-1234-5678</span>
         </div>
       </div>
     </div>
+
+    <!-- 관리자 카드 클릭 시 뜨는 모달 -->
+    <Modal :open="isEditModalOpen" @close="isEditModalOpen = false">
+      <div class="edit-modal-container">
+        <div class="manager-profile-image">
+          <img src="/public/assets/icons/ic-manager-profile.png" alt="관리자 프로필 이미지" />
+        </div>
+        <div class="manager-name">
+          <input type="text" />
+          <span>관리자</span>
+        </div>
+        <div class="manager-info-modal">
+          <span>이메일</span>
+          <p>test01@test.com</p>
+        </div>
+        <div class="manager-info-modal">
+          <span>연락처</span>
+          <input type="text" />
+        </div>
+        <div class="modal-button-container">
+          <button id="delete-button">계정삭제</button>
+          <Button id="edit-button">수정</Button>
+        </div>
+      </div>
+    </Modal>
   </AdminLayout>
 </template>
 
 <style scoped>
 .manage-list-container {
-    @apply flex flex-wrap gap-3 mt-[15px] ml-[4px] pb-[30px];
+  @apply flex flex-wrap gap-3 mt-[15px] ml-[4px] pb-[30px];
 }
 
 .card {
-    @apply rounded-[5px] w-[230px] h-[140px] cursor-pointer;
+  @apply rounded-[5px] w-[230px] h-[140px] cursor-pointer;
 }
 
 .manage-add-button-container {
-    @apply bg-[#F5F5F5] flex justify-center items-center;
+  @apply bg-[#F5F5F5] flex justify-center items-center;
 }
 
 .manage-add-button-container img {
-    @apply w-[45px];
+  @apply w-[45px];
 }
 
 .manager-info-card {
-    @apply bg-[#F8F8F8] px-[22px] pt-[23px] pb-[21px];
+  @apply bg-[#F8F8F8] px-[22px] pt-[23px] pb-[21px];
 }
 
 .card-top {
-    @apply flex justify-between items-center font-medium mb-[22px];
+  @apply flex justify-between items-center font-medium mb-[22px];
 }
 
 .card-top img {
-    @apply w-[16px] h-[16px] cursor-pointer;
+  @apply w-[16px] h-[16px] cursor-pointer;
 }
 
 .manager-info-row {
-    @apply flex items-center gap-3 mb-[8px];
+  @apply flex items-center gap-3 mb-[8px];
 }
 
 .manager-info-row span {
-    @apply text-[14px] max-w-[156px] truncate;
+  @apply text-[14px] max-w-[156px] truncate;
 }
 
 .manager-info-row img {
-    @apply w-[16px] h-[16px];
+  @apply w-[16px] h-[16px];
+}
+
+/* 관리자 카드 클릭 시 뜨는 모달 스타일 */
+.edit-modal-container {
+  @apply flex flex-col px-[20px] py-[10px];
+}
+
+.manager-profile-image img {
+  @apply w-[100px];
+}
+
+.manager-name {
+  @apply flex items-center gap-2 mt-[18px] mb-[20px] px-[8px];
+}
+
+.manager-name input {
+  @apply w-[60px] text-[18px] font-medium;
+}
+
+.manager-info-modal {
+  @apply flex gap-3 mb-[15px] px-[8px] text-[14px];
+}
+
+.manager-info-modal P {
+    @apply text-gray-dark;
+}
+
+.modal-button-container {
+  @apply mt-[40px] flex gap-3;
+}
+
+.modal-button-container button {
+  @apply rounded-[5px] text-center py-[11px] flex-1 text-[14px] font-medium;
+}
+
+#delete-button {
+  @apply bg-gray-deep text-text hover:bg-[#c8c8c8];
+}
+
+#edit-button {
+  @apply text-white px-0;
+}
+
+input {
+    @apply border-b border-gray-deep
 }
 </style>
