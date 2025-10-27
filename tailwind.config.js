0aae14c4c1783d95a8c07755d83cae182ac42ed3
somminn <cowmin104@gmail.com>
2025-10-27 15:57:44 +0900

    [Test] app.css 포함 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,css}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,css}',"./src/app.css",],
   theme: {
     extend: {
       colors: {
