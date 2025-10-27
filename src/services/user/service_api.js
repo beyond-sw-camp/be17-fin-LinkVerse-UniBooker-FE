d995a4b5612e02e58592f0ec1fbbe8f1335429b8
seoyoen22 <hsy2709@gmail.com>
2025-10-27 10:20:52 +0900

    [Feat] 유저 서비스 리스트 조회 api 연동

--- src/services/user/service_api.js
+++ src/services/user/service_api.js
@@ -13,6 +13,26 @@ const getServiceGroups = async () => {
   }
 }
 
+const getServiceGroupDetail = async (serviceGroupId) => {
+  try {
+    const response = await axiosInstance.get(`api/resource-group/${serviceGroupId}`)
+    return response.data.data
+  } catch (error) {
+    console.log('서비스 그룹 상세 조회 실패:', error)
+  }
+}
+
+const getServices = async (serviceGroupId) => {
+  try {
+    const response = await axiosInstance.get(`api/resource/group/${serviceGroupId}`)
+    return response.data.data.resources
+  } catch (error) {
+    console.log('서비스 그룹 목록 조회 실패:', error)
+  }
+}
+
 export default {
   getServiceGroups,
+  getServiceGroupDetail,
+  getServices,
 }
