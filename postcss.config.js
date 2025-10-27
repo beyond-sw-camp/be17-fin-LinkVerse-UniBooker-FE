87c91f9e87e8d6acf6da75b2b8bb4bbf5785add9
somminn <cowmin104@gmail.com>
2025-10-27 16:18:34 +0900

    [Fix] postcss.config.cjs 수정, vite.config.js 수정

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  plugins: {
-    // TODO : 버전에 맞는 걸로 남기기
-    //'@tailwindcss/ ': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import postcssConfig from './postcss.config.cjs'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  css: {
+    postcss: postcssConfig,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
