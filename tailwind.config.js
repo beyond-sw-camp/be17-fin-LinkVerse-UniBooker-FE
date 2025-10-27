c8c795725045be6f40807dfac7c9fc98050e43d5
somminn <cowmin104@gmail.com>
2025-10-27 16:06:30 +0900

    [Fix] 빌드 실패 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,css}',"./src/app.css",],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,css}'],
   theme: {
     extend: {
       colors: {
