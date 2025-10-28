868b9c6e3629805e4dcd01f03027d855cdc2c4be
thay123028 <thay123028@naver.com>
2025-10-28 21:06:15 +0900

    [Feat] 웹소켓 알림 도착 시 알림 아이콘 변경

---
+++ src/stores/notificationStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useNotificationStore = defineStore('notification', () => {
+  // 알림 도착 여부
+    const hasNotification = ref(false)
+
+  // 알림 도착 시 true로 설정
+  function notify() {
+      hasNotification.value = true
+  }
+
+  // 드롭다운 열면 false로 초기화
+  function reset() {
+    hasNotification.value = false
+  }
+
+  return { hasNotification, notify, reset }
+})
